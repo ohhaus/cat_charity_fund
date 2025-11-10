d194eca0907819bea92ab5e70dbacb7517ea19d4
ohhaus <ohhaus84@gmail.com>
2025-11-10 19:15:07 +0300

    fix: Исправлены теги в документации

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -9,11 +9,9 @@ router = APIRouter()
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix='/auth/jwt',
-    tags=['auth'],
 )
 
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix='/auth',
-    tags=['auth'],
 )
